75471f4334296899bd12cc52f77e0a766eeba785
xcc <1138084278@qq.com>
2022-03-31 09:59:25 +0800

    use basic middleware

--- README.md
+++ README.md
@@ -1,5 +1,17 @@
-## 一个基于开源项目realworld的api接口实现
+> 一个基于开源项目realworld的api接口实现
 
 - 项目地址：https://github.com/gothinkster/realworld
 
 - api文档地址：https://realworld-docs.netlify.app/docs/specs/backend-specs/endpoints
+
+----
+## Middleware
+- 请求体解析
+  - express.json()
+  - express.urlencoded()
+
+- 日志输出
+  - morgan()
+
+- 跨域请求
+  - cors()
--- app.js
+++ app.js
@@ -1,12 +1,23 @@
 const express = require('express')
+const morgan = require('morgan')
+const cors = require('cors')
+
 const config = require('./config')
 
 const PORT = config.PORT || 3000
 
 const app = express()
 
+app.use(morgan('dev'))
+app.use(express.json())
+app.use(cors())
+
 app.get('/', (request, response)=>{
     response.send(`Hello, world`)
+})
+
+app.post('/', (request, response)=>{
+    response.send(request.body)
 })
 
 
